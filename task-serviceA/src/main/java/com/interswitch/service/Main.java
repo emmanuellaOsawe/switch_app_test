ef331ee3a4187e7c984d5c13f5f79b12a6605f93
Ogheneyoma Okwori <Ogheneyoma.Okwori@Interswitchng.com>
2023-08-18 14:59:14 +0100

    added calculator method

--- task-serviceA/src/main/java/com/interswitch/service/Main.java
+++ task-serviceA/src/main/java/com/interswitch/service/Main.java
@@ -1,7 +1,21 @@
 package com.interswitch.service;
 
 public class Main {
+
+    private double secondNumber;
+    private double firstNumber;
+
+    void SimpleCalculator(double first, double second) {
+        this.firstNumber = first;
+        this.secondNumber = second;
+    }
+
+    public static double addTwoNumbers(double firstNumber, double secondNumber) {
+        return firstNumber + secondNumber;
+
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        addTwoNumbers(20, 60);
     }
 }
